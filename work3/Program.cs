66e7d2c423b4ccca854a567eb0f28d427e103cd9
Alex <Alex-f1n4@hotmail.com>
2022-02-05 21:57:34 +0300

    task3-4

---
+++ work3/Program.cs
@@ -0,0 +1,21 @@
+﻿// Даны два числа. Показать большее и меньшее число
+int a, b;
+Console.WriteLine("Введите первое число: ");
+a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+b = Convert.ToInt32(Console.ReadLine());
+ if (a > b)
+ 
+{
+ 
+      Console.WriteLine("Первое наибольшее число",a);
+ 
+}
+ 
+ else
+ 
+{
+ 
+      Console.WriteLine("Второе наибольшее число ", b);
+ 
+}
