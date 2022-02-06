a72b1a8e66958591936653a379cbf221d8eb0859
Alex <Alex-f1n4@hotmail.com>
2022-02-06 21:12:03 +0300

    06.02

---
+++ work5/Program.cs
@@ -0,0 +1,8 @@
+﻿// Найти максимальное из трех чисел
+int a = 4;
+int b = 5;
+int c = 6;
+int max = a ;
+if (b>max)max=b;
+if (c>max)max=c;
+System.Console.WriteLine(max);
---
+++ work7/Program.cs
@@ -0,0 +1,12 @@
+﻿// Выяснить является ли число чётным
+int x;
+System.Console.Write("Ввести число: ");
+x = int.Parse(Console.ReadLine());
+if ((x % 2) == 0)
+{
+    Console.WriteLine("Чётное");
+}
+else
+{
+    Console.WriteLine("Нечетное");
+}
