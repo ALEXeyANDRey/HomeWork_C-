c5cd7f946e6f1c9a0ab580eeaee1913ce2095ffc
Alex <Alex-f1n4@hotmail.com>
2022-03-06 10:03:49 +0300

    5354

---
+++ work53/Program.cs
@@ -0,0 +1,40 @@
+﻿// В двумерном массиве показать позиции числа, заданного пользователем или указать, что такого элемента нет
+Input(out int x, out int y);
+int[,] xy = new int [x,y];
+FillArray (xy);
+System.Console.WriteLine();
+Solve (xy);
+
+void Input (out int x,out int y)
+{
+    System.Console.Write("Кол-во строк - ");
+    x = int.Parse(Console.ReadLine());
+    System.Console.Write("Кол-во столбов - ");
+    y = int.Parse(Console.ReadLine());
+}
+void FillArray (int [,] xy)
+{
+    for (int i = 0; i < xy.GetLength(0); i++)
+    {
+        for (int j = 0 ; j < xy.GetLength(1); j++)
+        {
+            xy[i,j] = new Random().Next(1,10);
+            System.Console.Write($"{xy[i,j]} ");
+        }
+       System.Console.WriteLine();
+    }
+}
+void Solve (int [,] xy)
+{
+     System.Console.Write("Число - ");
+    int a = int.Parse(Console.ReadLine());
+    for (int i = 0; i < xy.GetLength(0);i++)
+    {
+        for (int j =0; j < xy.GetLength(1);j++)
+        {
+            if (a==xy[i,j]) System.Console.WriteLine("есть на позициях: строка - {0}, столбец - {1} ",i,j);
+            
+        }
+        
+    }
+}
---
+++ work54/Program.cs
@@ -0,0 +1,42 @@
+﻿// В матрице чисел найти сумму элементов главной диагонали
+Input(out int x, out int y);
+int[,] xy = new int [x,y];
+FillArray (xy);
+System.Console.WriteLine();
+
+Solve (out int sum);
+
+
+void Input (out int x,out int y)
+{
+    System.Console.Write("Кол-во строк - ");
+    x = int.Parse(Console.ReadLine());
+    System.Console.Write("Кол-во столбов - ");
+    y = int.Parse(Console.ReadLine());
+}
+void FillArray (int [,] xy)
+{
+    for (int i = 0; i < xy.GetLength(0); i++)
+    {
+        for (int j = 0 ; j < xy.GetLength(1); j++)
+        {
+            xy[i,j] = new Random().Next(1,10);
+            System.Console.Write($"{xy[i,j]} ");
+        }
+       System.Console.WriteLine();
+    }
+}
+void Solve(out int sum)
+{
+    sum = 0;
+    for (int i = 0; i < xy.GetLength(0); i++)
+    {
+        for (int j = 0 ; j < xy.GetLength(1); j++)
+        {
+            if (i==j) 
+            sum += xy[i,j];
+            
+        }
+    }
+    System.Console.WriteLine($"Сумма чисел по диагонали равна {sum} ");
+}
